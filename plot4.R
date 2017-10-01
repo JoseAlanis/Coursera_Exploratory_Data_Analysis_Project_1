46ee756d1c4c7d51ca9733d0ac33cd5640c3fc85
Jose Alanis <joialanisson@gmail.com>
2017-10-02 00:32:59 +0200

    added plot 4

---
+++ plot4.R
@@ -0,0 +1,66 @@
+#####                   plot4.R 
+#####     for Coursera Explanatory Data Analysis
+#####               Course project 1
+
+setwd('~/Documents/Coursera/04_Exploratory_Data_Analysis/Week_1/')
+
+# Read in file
+dat <- read.table('household_power_consumption.txt',
+                  header = T, 
+                  sep = ';', 
+                  stringsAsFactors = F, 
+                  colClasses = c(rep('character', 2), 
+                                 rep('numeric', 7)), 
+                  na.strings = '?')
+
+# Column dat$Date to class date
+dat$Date <- as.Date(dat$Date, 
+                    format = '%d/%m/%Y')
+
+# Select dates 2007-02-01 and 2007-02-02
+dat <- dat[dat$Date == '2007-02-01' | dat$Date == '2007-02-02', ]
+
+# Create column DateTime with class date
+dat$DateTime <- paste(dat$Date, dat$Time, sep = ' ')
+dat$DateTime <- strptime(dat$DateTime, format = '%Y-%m-%d %H:%M:%S')
+
+# Set graphic device and plot Global Active Power
+png('plot4.png', width = 480, height = 480)
+# Set plot parameters
+par(mfrow = c(2, 2))
+# plot 1
+plot(y = dat$Global_active_power,
+     x = dat$DateTime,
+     type = 'l', 
+     xlab = '',
+     ylab = 'Global Active Power')
+# plot 2
+plot(y = dat$Voltage,
+     x = dat$DateTime,
+     type = 'l', 
+     xlab = 'datetime',
+     ylab = 'Voltage')
+# plot 3
+plot(x = dat$DateTime,  
+     y = dat$Sub_metering_1, type = 'l', 
+     ylab = 'Energy sub metering', 
+     col = 'black',
+     xlab = '')
+lines(x = dat$DateTime,  
+      y = dat$Sub_metering_2, 
+      col='red')
+lines(x = dat$DateTime,  
+      y = dat$Sub_metering_3, 
+      col='blue')
+legend('topright', c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'),
+       lty = 1,
+       col = c('black', 'red', 'blue'), 
+       bty = 'n')
+# plot 4
+plot(y = dat$Global_reactive_power,
+     x = dat$DateTime,
+     type = 'l', 
+     xlab = 'datetime',
+     ylab = 'Global_reactive_power')
+
+dev.off() # Close device and finalise plot
